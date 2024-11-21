657f3a90ee432a48d37f404ef9e06525c029c1e8
“Diana <dinajahna@gmail.com>
2024-11-21 17:15:11 -0600

    Deleted unused import in my StepDef

--- src/test/java/stepdefinitions/Items_StepDef.java
+++ src/test/java/stepdefinitions/Items_StepDef.java
@@ -5,7 +5,6 @@ import org.junit.Assert;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.ItemsPage;
